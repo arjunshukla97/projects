d2a8f8239aaa64b85e779c38bab1c4b71d128250
arjunshukla97 <40526155+arjunshukla97@users.noreply.github.com>
2018-12-11 20:19:01 +0530

    Add files via upload

---
+++ course-api-data/src/main/java/com/arjun/springboot/topic/TopicRepository.java
@@ -0,0 +1,10 @@
+package com.arjun.springboot.topic;
+
+import com.arjun.springboot.topic.Topic;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String>{
+
+
+
+}
